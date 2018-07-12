3d0ffedc474cea504df5dd40158b1654dcaa1d59
Germey <cqc@cuiqingcai.com>
2018-07-13 02:57:53 +0800

    update middleware

--- scrapycrawlspider/spiders/china.py
+++ scrapycrawlspider/spiders/china.py
@@ -139,8 +139,8 @@ class ChinaSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow='article\/.*\.html',
                            restrict_xpaths='//div[@id="left_side"]//div[@class="con_item"]'),
-             render=True, callback='parse_item'),
-        # Rule(LinkExtractor(restrict_xpaths='//div[@id="pageStyle"]//a[contains(., "下一页")]'))
+             render=True, dont_retry=True, callback='parse_item'),
+        Rule(LinkExtractor(restrict_xpaths='//div[@id="pageStyle"]//a[contains(., "下一页")]'))
     )
     
     def parse_item(self, response):
